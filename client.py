59be844ad0348565fff398f4f800d8d26e2679b8
Olt Latifi <oltlatifi2003@gmail.com>
2022-01-06 11:46:34 +0100

    server sends a message back to client

--- client.py
+++ client.py
@@ -19,6 +19,7 @@ def send(msg):
     send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
     client.send(message)
+    print(client.recv(2048).decode(FORMAT))
 
 send("Hi mom")
 send("Hi mom")
